5456605b73815dd25c7d805aa845e649545693f9
amtogg <nikitaamto@gmail.com>
2024-02-11 15:48:14 +0300

    add constans

---
+++ constans/constants.java
@@ -0,0 +1,16 @@
+package constans;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class constants {
+    public static final List RUS_ALPHABET = Arrays.asList('а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'к', 'л', 'м', 'н', 'о',
+            'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'я');
+    /*public static final String [] RUS_ALPHABET2 = new String[] {"а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к",
+            "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"};
+    public static final String [] PUNCTUATION_MARKS = new String[] {".", ",","'", ":", "!", "?", " ", "\""};*/
+}
+
+
